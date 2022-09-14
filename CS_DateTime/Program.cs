596bcdd3745d3c77790cef625a0b969cb91747d1
Nayan Ranjane <nayan.ranjane@coditas.com>
2022-09-14 17:38:49 +0530

    DateTime

---
+++ CS_DateTime/Program.cs
@@ -0,0 +1,38 @@
+﻿// Working with DateTime
+
+
+var dateTime = new DateTime(2018,12,12);
+
+var now = DateTime.Now;
+
+var today = DateTime.Today;
+
+Console.WriteLine(now);
+
+Console.WriteLine(today);
+
+// to get the Perticular one
+
+Console.WriteLine("Year = " + now.Year);
+
+Console.WriteLine("Month = "+now.Month);
+
+Console.WriteLine("Day = "+now.Year);
+
+Console.WriteLine("Current time in Hour = "+now.Hour);
+
+// long date string
+
+Console.WriteLine("Long Date string " +now.ToLongDateString());
+
+Console.WriteLine("Time = " +now.ToLongTimeString() );
+
+
+// both date and time
+
+Console.WriteLine("Both =" +now.ToString());
+Console.WriteLine("yyyy - MM - dd format =" + now.ToString("yyyy-MM-dd"));
+
+
+
+
--- CS_List/Program.cs
+++ CS_List/Program.cs
@@ -1,5 +1,5 @@
 ﻿
-List<int> integerList = new List<int>() { 123,23334,223,2345,235,645};
+List<int> integerList = new List<int>() { 123,23334,223,2345,223,645};
 
 // We can Add item by Add method
 
@@ -15,3 +15,24 @@ foreach(var integer in integerList)
     Console.WriteLine(integer);
 }
 
+// find the index of the method
+
+
+Console.WriteLine("Index of the integer 2 is = " + integerList.IndexOf(2));
+
+// finding the last index of the element
+
+
+
+Console.WriteLine("Last Index of the integer 223 is = " + integerList.LastIndexOf(223));
+
+
+// Removing the element of the array
+
+integerList.Remove(223);
+Console.WriteLine("After Removing the item 223");
+
+foreach (var item in integerList)
+{
+    Console.WriteLine(item);
+}
