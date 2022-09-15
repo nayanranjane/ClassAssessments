5c706a0d3fa088ec1bfd3bf104f2bc02c2e03bf7
Nayan Ranjane <nayan.ranjane@coditas.com>
2022-09-15 16:55:32 +0530

    Completed

--- CS_ExerciseModuleNo_8Q3/Program.cs
+++ CS_ExerciseModuleNo_8Q3/Program.cs
@@ -2,4 +2,19 @@
 
 Console.WriteLine("Enter the hour and minuts");
 
-DateTime 
+string input = Console.ReadLine();
+string[] inputArray = input.Split(':');
+
+
+
+var Time = TimeSpan.Parse(Console.ReadLine());
+
+Console.WriteLine(Time.Hours);
+if (Convert.ToInt32(inputArray[0]) <00 || Convert.ToInt32(inputArray[0]) > 24)
+{
+    Console.WriteLine("Invalid Time");
+}
+else
+{
+    Console.WriteLine("OK");
+}
