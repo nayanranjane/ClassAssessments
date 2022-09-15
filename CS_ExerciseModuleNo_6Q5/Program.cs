345714904021e82b90c33473531eab99fdf37533
Nayan Ranjane <nayan.ranjane@coditas.com>
2022-09-15 15:20:03 +0530

    Exercise

---
+++ CS_ExerciseModuleNo_6Q5/Program.cs
@@ -0,0 +1,21 @@
+﻿string input = Console.ReadLine();
+
+var inputArray = input.Split(" ");
+
+string[] arrayInput = new string[5];
+if(inputArray.Length > 5)
+{
+    Console.WriteLine("Enter the correct string");
+    input = Console.ReadLine();
+    arrayInput= input.Split(" ");
+}
+int[] intArray = new int[5];
+
+for(int i = 0; i < intArray.Length; i++)
+{
+    intArray[i] = Convert.ToInt32(arrayInput[i]);
+}
+Array.Sort(intArray);
+Console.WriteLine(intArray[0]);
+Console.WriteLine(intArray[1]);
+Console.WriteLine(intArray[2]);
---
+++ CS_ExerciseModuleNo_6Q6/Program.cs
@@ -0,0 +1,19 @@
+﻿//1 - Write a program and ask the user to enter a few numbers separated by a hyphen. Work out if the numbers are consecutive. For example, if the input is "5-6-7-8-9" or "20-19-18-17-16", display a message: "Consecutive"; otherwise, display "Not Consecutive".
+
+
+string input = Console.ReadLine();
+
+var inputArray = input.Split('-');
+
+int[] intArray = new int[inputArray.Length];
+int diference = intArray[1] - intArray[0];
+for (int i = 0; i < intArray.Length-1; i++)
+{
+    if (intArray[i + 1] - intArray[i] != diference)
+    {
+        Console.WriteLine("Non Consecutive");
+        return;
+    }
+}
+Console.WriteLine("Consecutive");
+
