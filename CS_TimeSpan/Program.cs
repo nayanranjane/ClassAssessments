3d527aa07daca53757f179ff4f7a135e8b1ddb62
Nayan Ranjane <nayan.ranjane@coditas.com>
2022-09-15 10:28:28 +0530

    TimeSpan Completed

---
+++ CS_TimeSpan/Program.cs
@@ -0,0 +1,43 @@
+﻿
+// Time span
+
+var timeSpan = new TimeSpan(1, 4, 7);
+
+
+// Above value are given as hour minuts and second 
+
+// if the value is not given it will take 0;
+
+
+var newSpan = new TimeSpan(2, 6, 9);
+
+
+var forTwoHour = new TimeSpan(2, 0, 0);
+
+// Instead of writting the above code we can write the following code for the
+
+
+var forTwoHour2 = TimeSpan.FromHours(2);
+
+var startingTime = DateTime.Now;
+
+var endingTime = DateTime.Now.AddHours(2);
+
+var durationOfTime = endingTime - startingTime;
+Console.WriteLine("Time span between the two value ");
+
+Console.WriteLine(durationOfTime);
+
+// Timespan class has some of the properties 
+
+
+// we have created the object of the Timespan class in the 1st part
+
+
+
+Console.WriteLine("Hour of the Timespan = " +timeSpan.Hours);
+
+
+Console.WriteLine("Minuts of the Timespan = "+timeSpan.Minutes);
+
+Console.WriteLine("Seconds of the Timespan = "+timeSpan.Seconds);
